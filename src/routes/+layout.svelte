849f2248e6c074942fa85dba911a2a362e547445
koen1711 <koenvurk1711@gmail.com>
2025-11-12 16:09:50 +0100

    chore: add 404 error

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	export const prerender = true
-
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import ThemeContext from "$lib/context/theme/ThemeContext.svelte";
