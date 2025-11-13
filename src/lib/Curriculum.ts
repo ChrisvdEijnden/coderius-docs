80fdf7fc57e21392b86173cacc0af1a199ca3299
Marten Postma <postmam@corderius.nl>
2025-11-13 11:23:21 +0100

    added godot (#1)
    * added godot
    
    * removed yarn.lock
    
    * chore: add order
    
    * chore: add nested order
    
    * fix: add .editorconfig
    
    * chore: add linter & formatter
    
    * chore: add biome as devDep
    
    * chore: fix scripts
    
    ---------
    
    Co-authored-by: Koen <98043234+koen1711@users.noreply.github.com>
    Co-authored-by: koen1711 <koenvurk1711@gmail.com>

--- src/lib/Curriculum.ts
+++ src/lib/Curriculum.ts
@@ -1,43 +1,68 @@
 export type Activity = {
-    title: string;
-    labels: string[];
-    level: "Beginner" | "Medium" | "Advanced";
-    link: string;
-    order: number;
-		ownMaterials: boolean;
-}
+	title: string;
+	labels: string[];
+	level: "Beginner" | "Medium" | "Advanced";
+	link: string;
+	order: {
+		vwo: number;
+		havo: number;
+	};
+	ownMaterials: boolean;
+};
 
 export let curriculum: Activity[] = [
-    {
-        title: "Python Play",
-        labels: ["Python", "Game Development", "2D"],
-        level: "Beginner",
-        link: "https://play.coderius.nl/",
-        order: 1,
-				ownMaterials: true
-    },
-    {
-        title: "Robotica",
-        labels: ["Robotics", "Python", "Building"],
-        level: "Beginner",
-        link: "https://robotica.coderius.nl/",
-        order: 2,
-				ownMaterials: true
-    },
-    {
-        title: "Damn Vulnerable Web Application",
-        labels: ["Security", "Web"],
-        level: "Medium",
-        link: "https://dvwa.coderius.nl/",
-        order: 5,
-				ownMaterials: false
-    }
+	{
+		title: "Python Play",
+		labels: ["Python", "Game Development", "2D"],
+		level: "Beginner",
+		link: "https://play.coderius.nl/",
+		order: {
+			vwo: 2,
+			havo: 2,
+		},
+		ownMaterials: true,
+	},
+	{
+		title: "Robotica",
+		labels: ["Robotics", "Python"],
+		level: "Beginner",
+		link: "https://robotica.coderius.nl/",
+		order: {
+			vwo: 3,
+			havo: 3,
+		},
+		ownMaterials: true,
+	},
+	{
+		title: "Vulnerable Web Application",
+		labels: ["Security", "Web"],
+		level: "Advanced",
+		link: "https://dvwa.coderius.nl/",
+		order: {
+			vwo: 5,
+			havo: 5,
+		},
+		ownMaterials: true,
+	},
+	{
+		title: "Godot Tutorial",
+		labels: ["Game Development", "Godot", "2D", "3D", "GDScript"],
+		level: "Medium",
+		link: "https://godot.coderius.nl/",
+		order: {
+			vwo: 6,
+			havo: 6,
+		},
+		ownMaterials: true,
+	},
 ];
 
-export let ownCurriculum: Activity[] = curriculum.filter(activity => activity.ownMaterials);
+export let ownCurriculum: Activity[] = curriculum.filter(
+	(activity) => activity.ownMaterials,
+);
 
 export let levelColors: { [key in Activity["level"]]: string } = {
-    "Beginner": "green",
-    "Medium": "orange",
-    "Advanced": "red"
-}
+	Beginner: "green",
+	Medium: "orange",
+	Advanced: "red",
+};
